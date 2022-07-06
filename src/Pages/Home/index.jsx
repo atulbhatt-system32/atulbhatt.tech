0f2bc2088550777747b8001c6d167d0786743fe7
pragati patel <peehupatel1998@gmail.com>
2022-07-07 01:24:43 +0530

    add intro section for mobile view, added string flie and custom css in tailwind config file

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import { Footer } from "../../Components";
+import IntroSection from "./IntroSection";
 export default function Home() {
   return (
     <div>
       <section>
+      <IntroSection/>
         <Footer />
       </section>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontSize: {
+      "14px":"14px",
+      "18px":"18px",
+      "32px": "32px",
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
@@ -14,6 +19,19 @@ module.exports = {
       silver: "#ecebff",
       "bubble-gum": "#ff77e9",
       bermuda: "#78dcca",
+      primary:{
+        "blue-200":"#5454D4",
+        "blue-500":"rgba(84, 84, 212, 1)",
+      },
+      white:{
+        "900":"#ffffff",
+      },
+      gray:{
+        "200":"rgba(174, 174, 174, 1)",
+      },
+      black:{
+        "500":"#575559"
+      }
     },
   },
   plugins: [],

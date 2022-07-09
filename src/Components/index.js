ec07387f1f126927775b1ec7e7a2278a4a5babb1
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-07-09 13:17:27 +0530

    add: header

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,4 +3,5 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Container from "./Container";
 import Button from "./Button";
-export { Card, Footer, Navbar, Container, Button };
+import Header from "./Header";
+export { Card, Footer, Navbar, Container, Button, Header };

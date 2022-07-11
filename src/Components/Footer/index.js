98e553209807e95ce1367dd43c11e4c0eac401ed
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-07-11 12:59:58 +0530

    style: footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import {
-  Facebook,
-  Instagram,
-  Twitter,
-  Linkedin,
-} from "../../Assets/images/footer";
+import styles from "./Footer.module.scss";
+import { Facebook, Instagram, Twitter, Linkedin } from "../../Assets/images";
 const Footer = () => {
   return (
-    <footer className="bg-midnight px-7 py-10 2xl:px-24 md:px-16 2xl:py-10 md:py-8   ">
+    <footer className={styles.footer}>
       {/* CONTACT */}
-      <div className="border-b border-solid border-white pb-6 flex flex-col gap-4 text-white">
+      <div className={styles.footer__wrap}>
         <div className="flex flex-col gap-6 pb-9">
           <div className="opacity-90 font-bold text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl">
             Contact

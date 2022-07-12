d8542320b2dfb622fe16585b425b5a6c49319523
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-07-12 13:41:39 +0530

    style: project

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = (props) => {
-  const classes = props.className + " rounded-xl drop-shadow-2xl bg-white-900";
+  const classes = props.className + " rounded-xl drop-shadow-2xl";
   return <div className={classes}>{props.children}</div>;
 };
 export default Card;

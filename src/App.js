d2af7bb9315a236b3ee3527785fa3d02908ea890
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-07-16 10:03:40 +0530

    Routing mapping

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import { Home, AboutUs, Blogs } from "./Pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "pure-react-carousel/dist/react-carousel.es.css";
-
+import { routes } from "./static.db";
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/blogs" element={<Blogs />} />
+          <Route exact path={routes.home.path} element={<Home />} />
+          <Route path={routes.aboutMe.path} element={<AboutUs />} />
+          <Route path={routes.content.path} element={<Blogs />} />
         </Routes>
       </Router>
     </div>

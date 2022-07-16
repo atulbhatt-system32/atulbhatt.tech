ab726a9a295e120dfb574dbc444a1e2acfd25f33
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-07-16 13:16:40 +0530

    style: blog

--- src/Pages/Content/Blog/index.jsx
+++ src/Pages/Content/Blog/index.jsx
@@ -11,28 +11,32 @@ const Blog = () => {
             <img src={Blog1} alt="Blog1" />
           </figure>
         </aside>
-        <header className={styles.card_header}>
-          <h3 className={styles.card_title}>Blog Title</h3>
-        </header>
-        <div className={styles.card_body}>
-          <p className={styles.card_copy}>
-            Lorem ipsum dolor sit ametcons ectetur adipiscing elit.Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
-            amet, consectetur adipiscing elit.
-          </p>
-          <p className={styles.card_tag}>
-            tags- #react #kyu #ptanahi #ptanah i#ptanahi# ptanahi #ptanahi
-            #ptanahi
-          </p>
-        </div>
-        <div className={styles.card_footer}>
-          <div className={styles.card_watched}>
-            <img src={Watched} alt="watched" />
-            <span>34 People</span>
+        <div className={styles.card_content}>
+          <header className={styles.card_header}>
+            <h3 className={styles.card_title}>Blog Title</h3>
+          </header>
+          <div className={styles.card_body}>
+            <p className={styles.card_copy}>
+              Lorem ipsum dolor sit ametcons ectetur adipiscing elit.Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit.
+            </p>
           </div>
-          <div className={styles.card_liked}>
-            <img src={Heart} alt="liked" />
-            <span>34 People</span>
+          <div className={styles.card_tag}>
+            <p>
+              tags- #react #kyu #ptanahi #ptanah i#ptanahi# ptanahi #ptanahi
+              #ptanahi
+            </p>
+          </div>
+          <div className={styles.card_footer}>
+            <div className={styles.card_watched}>
+              <img src={Watched} alt="watched" />
+              <span>34 People</span>
+            </div>
+            <div className={styles.card_liked}>
+              <img src={Heart} alt="liked" />
+              <span>34 People</span>
+            </div>
           </div>
         </div>
       </article>

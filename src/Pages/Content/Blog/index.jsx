16b781d085244cae62404b1e15538ad6154bf727
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-07-16 12:00:51 +0530

    add: blog

---
+++ src/Pages/Content/Blog/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "../Content.module.scss";
+import { Blog1, Watched, Heart } from "../../../Assets/images";
+import { Card } from "../../../Components";
+const Blog = () => {
+  return (
+    <Card>
+      <article className={styles.card}>
+        <aside className={styles.card_aside}>
+          <figure className={styles.card_figure}>
+            <img src={Blog1} alt="Blog1" />
+          </figure>
+        </aside>
+        <header className={styles.card_header}>
+          <h3 className={styles.card_title}>Blog Title</h3>
+        </header>
+        <div className={styles.card_body}>
+          <p className={styles.card_copy}>
+            Lorem ipsum dolor sit ametcons ectetur adipiscing elit.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit.
+          </p>
+          <p className={styles.card_tag}>
+            tags- #react #kyu #ptanahi #ptanah i#ptanahi# ptanahi #ptanahi
+            #ptanahi
+          </p>
+        </div>
+        <div className={styles.card_footer}>
+          <div className={styles.card_watched}>
+            <img src={Watched} alt="watched" />
+            <span>34 People</span>
+          </div>
+          <div className={styles.card_liked}>
+            <img src={Heart} alt="liked" />
+            <span>34 People</span>
+          </div>
+        </div>
+      </article>
+    </Card>
+  );
+};
+
+export default Blog;

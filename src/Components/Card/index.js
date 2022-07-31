a36a5f7af2b01042b2eecf8dc1dc142f45aacbc0
atulbhatt-system32 <atulbhatt98@gmail.com>
2022-07-31 21:12:59 +0530

    Blog integration

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Card = (props) => {
-  const classes = props.className + " rounded-xl drop-shadow-2xl";
+  const classes =
+    props.className + " rounded-xl drop-shadow-2xl cursor-pointer";
   const data = { ...props.data };
   return (
     <div className={classes} onClick={() => window.open(data.url)}>

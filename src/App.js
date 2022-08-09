dc2c7235fd6bbf8018ec420ff9903f40e8f751ee
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-09 14:58:18 +0530

    add: not Found

--- src/App.js
+++ src/App.js
@@ -10,15 +10,18 @@ function App() {
   const { theme, changeTheme, toggleTheme } = useTheme();
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path={routes.home.path} element={<Home />} />
-          <Route exact path={routes.aboutMe.path} element={<AboutUs />} />
-          <Route exact path={routes.content.path} element={<Content />} />
-          <Route exact path={routes.skills.path} element={<Skills />} />
-        </Routes>
-      </Router>
+    <div className={`App ${theme}`}>
+      <ThemeContext.Provider value={{ theme, changeTheme, toggleTheme }}>
+        <Router>
+          <Routes>
+            <Route exact path={routes.home.path} element={<Home />} />
+            <Route exact path={routes.aboutMe.path} element={<AboutUs />} />
+            <Route exact path={routes.content.path} element={<Content />} />
+            <Route exact path={routes.skills.path} element={<Skills />} />
+            <Route exact path={routes.notFound.path} element={<NotFound />} />
+          </Routes>
+        </Router>
+      </ThemeContext.Provider>
     </div>
   );
 }

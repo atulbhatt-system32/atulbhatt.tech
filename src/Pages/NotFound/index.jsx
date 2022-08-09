eaf2dff5c6b2aae1868264130b62627f1a8e0ec9
Vivek Bhatt <vivekbhatt618@gmail.com>
2022-08-09 18:56:34 +0530

    style: not found

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
-
+import Navbar from "../../Components/Navbar/index";
+import error from "../../Assets/images/notFound/error.svg";
+import styles from "./NotFound.module.scss";
 const NotFound = () => {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.not_found_wrap}>
+      <Navbar />
+      <div className={styles.not_found}>
+        <div className={styles.not_found_head}>
+          <img src={error} alt={styles.error_img} />
+        </div>
+        <div className={styles.not_found_body}>
+          <p>
+            JOJO try to fix things... <span>Say hi to JOJO</span> 👋
+          </p>
+          <p>Don't worry there is a way to go back home!</p>
+          <div className={styles.not_found_btn}>
+            <button>Back to home </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
